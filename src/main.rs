1918047e44b5c78019ae5426c5f3434e00f1af77
Dhananjay-JSR <ajaysenday@gmail.com>
2023-11-18 22:29:55 +0530

    Added Gracefull Shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process::exit;
 use axum::{routing::get, Router, ServiceExt};
 use simple_logger::SimpleLogger;
+use tokio::signal;
 use log::{info, trace, warn};
 #[tokio::main]
  async fn main() {
@@ -9,7 +10,7 @@ use log::{info, trace, warn};
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     match axum::Server::try_bind(&"0.0.0.0:3000".parse().unwrap()){
         Ok(ServerBuilder) => {
-            ServerBuilder.serve(app.into_make_service())
+            ServerBuilder.serve(app.into_make_service()).with_graceful_shutdown(shutdown_signal())
                 .await
                 .unwrap();
         }
@@ -21,4 +22,32 @@ warn!("Unable to Bind Ports , Please Kill the Process Acquiring Port 3000 and Tr
     // build our application with a single route
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
+
+
+}
+
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+        let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
